7a04aad0f895c54ce6dc633707f773f09aecc723
Marc Sanuy <45559432+marcsanuy@users.noreply.github.com>
2019-08-18 12:03:10 +0200

    Build Folder

---
+++ build/precache-manifest.1e1ddbb06ab5bfa3174b59fe259e8ea2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671d15bb70b28f670c6b87a9e7b36f72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78ca82cb7b8e8e39b2ea",
+    "url": "/static/css/main.1ae25f70.chunk.css"
+  },
+  {
+    "revision": "442324eb90d61745d4a3",
+    "url": "/static/js/2.2e69c9a5.chunk.js"
+  },
+  {
+    "revision": "78ca82cb7b8e8e39b2ea",
+    "url": "/static/js/main.89a90c4e.chunk.js"
+  },
+  {
+    "revision": "e4d8c659f867ba67f510",
+    "url": "/static/js/runtime~main.4a705023.js"
+  }
+]);
